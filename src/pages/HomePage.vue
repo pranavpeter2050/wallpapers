c619d313c6eadd81968cba59917ab7a2bd5a62d5
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-03-23 00:04:24 +0530

    Added some placeholders and styles to Home page.

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,109 @@
 <template>
-  <q-page> Home </q-page>
+  <q-page>
+    <div class="q-pa-lg">
+      <div class="d-grid q-gutter-md">
+        <div class="d-grid-item">
+          <q-card class="my-card">
+            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+              <div class="absolute-bottom text-subtitle2 text-center q-py-5px">
+                Title
+              </div>
+            </q-img>
+          </q-card>
+        </div>
+
+        <div class="d-grid-item">
+          <q-card class="my-card">
+            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+              <div class="absolute-bottom text-subtitle2 text-center">
+                Title
+              </div>
+            </q-img>
+          </q-card>
+        </div>
+
+        <div class="d-grid-item">
+          <q-card class="my-card">
+            <q-img src="https://randomuser.me/api/portraits/thumb/men/75.jpg">
+              <div class="absolute-bottom text-subtitle2 text-center">
+                Title
+              </div>
+            </q-img>
+          </q-card>
+        </div>
+
+        <div class="d-grid-item">
+          <q-card class="my-card">
+            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+              <div class="absolute-bottom text-subtitle2 text-center">
+                Title
+              </div>
+            </q-img>
+          </q-card>
+        </div>
+
+        <div class="d-grid-item">
+          <q-card class="my-card">
+            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+              <div class="absolute-bottom text-subtitle2 text-center">
+                Title
+              </div>
+            </q-img>
+          </q-card>
+        </div>
+
+        <div class="d-grid-item">
+          <q-card class="my-card">
+            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+              <div class="absolute-bottom text-subtitle2 text-center">
+                Title
+              </div>
+            </q-img>
+          </q-card>
+        </div>
+
+        <div class="d-grid-item">
+          <q-card class="my-card">
+            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+              <div class="absolute-bottom text-subtitle2 text-center">
+                Title
+              </div>
+            </q-img>
+          </q-card>
+        </div>
+
+        <div class="d-grid-item">
+          <q-card class="my-card">
+            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+              <div class="absolute-bottom text-subtitle2 text-center">
+                Title
+              </div>
+            </q-img>
+          </q-card>
+        </div>
+
+        <div class="d-grid-item">
+          <q-card class="my-card">
+            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+              <div class="absolute-bottom text-subtitle2 text-center">
+                Title
+              </div>
+            </q-img>
+          </q-card>
+        </div>
+
+        <div class="d-grid-item">
+          <q-card class="my-card">
+            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+              <div class="absolute-bottom text-subtitle2 text-center">
+                Title
+              </div>
+            </q-img>
+          </q-card>
+        </div>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
@@ -9,3 +113,37 @@ export default defineComponent({
   name: "HomePage",
 });
 </script>
+
+<style>
+.d-grid {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  grid-template-rows: 320px;
+}
+
+.d-grid-item {
+  /* max-width: 630px; */
+  /* max-height: 1587px; */
+}
+
+@media (min-width: 768px) {
+  .d-grid {
+    grid-template-columns: auto auto auto auto;
+  }
+}
+@media (min-width: 1160px) {
+  .d-grid {
+    grid-template-columns: auto auto auto auto auto;
+  }
+}
+@media (min-width: 1024px) {
+  .d-grid {
+    grid-template-columns: auto auto auto auto auto auto;
+  }
+}
+@media (min-width: 1515px) {
+  .d-grid {
+    grid-template-columns: auto auto auto auto auto auto auto;
+  }
+}
+</style>
